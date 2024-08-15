e9c41b616f10326fbc35c07b8f2e39aec0edde66
info7tik <info7tik@gmail.com>
2024-08-15 12:02:31 +0200

    install capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.grandma',
+  appName: 'grandma',
+  webDir: 'dist/grandma/browser'
+};
+
+export default config;
